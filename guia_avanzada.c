eacc6c11437234cba4558468dca37a997d54e7f6
Mirunr <miriam.enr0@gmail.com>
2026-07-21 17:29:17 -0300

    Update guia_avanzada.c
    llegamos hasta la pag 21 aprox
    
    rever los conceptos de static

--- guia_avanzada.c
+++ guia_avanzada.c
@@ -1,5 +1,6 @@
 #include <stdio.h> 
-
+#include <stdint.h>
+#include <string.h>
 //Definir una estructura monstruo t que contenga los siguientes campos:
 //nombre (string)
 //vida (entero)
@@ -67,11 +68,145 @@ int punterossss(){
 // *p de esta forma accedemos a la dirección de memoria guardada en el puntero p, es decir en este caso accedemos al valor de x 
 
 
-int main(){
+/*int snippet12(){
     int8_t memoria[2] = {0x20,0x28};
     int8_t *x = (int8_t*) 0x20;
     int8_t *y = (int8_t*) 0x28;
     printf("Dir de x: %p Valor: %d\n", (void*) x, *x);
     printf("Dir de y: %p Valor: %d\n", (void*) y, *y);
+
+    return 0;
+    }
+*/
+
+/*SEGMENTATION FAULT 
+que un puntero apunte a un lugar de memoria no definido no trae ningún problema, el problema sería
+si queremos leer o escribir ese lugar de memoria. En ese caso, el comportamiento es indefinido,
+porque básicamente no sabemos que hay en esa dirección de memoria. Puede haber memoria
+de nuestro proceso, o puede haber memoria de otro proceso. En caso que haya memoria de otro
+proceso, el sistema operativo nos va a arrojar un error de Segmentation fault.
+En caso que haya memoria de nuestro proceso, el comportamiento es indefinido: el programa puede seguir
+ejecutándose con errores lógicos o puede terminar abruptamente en un Segmentation fault,
+según la operación y el tipo de memoria accedida*/
+
+int stringss(){
+    char *str1 = "Hola";
+    char str2[] = "Hola";
+    str1 = "Hola, cómo estás?";
+/* *str1 = "Hola, cómo estás?"; // ❌ Error
+Al poner el asterisco * delante de str1, estás desreferenciando el puntero. Es decir, no estás modificando a dónde apunta el puntero, 
+sino el primer carácter individual (char) al que apunta. Por eso el compilador te advierte que le estás asignando una dirección
+de memoria (char *) a un carácter sencillo (char).*/
+    str2[0] = 'j';
+    //str2[4] = ' ';
+    //str2[5] = 'c';
+    printf("%s\n", str1);
+    printf("%s\n", str2);
+    return 0;
+}
+
+
+/* Cuando se declara un string como puntero la variable que guarda la memoria nada más se guarda en el stack mientras que lo demás, digamos el 
+contenido del string está en una parte de la memoria que se llama .rodata por read only, por lo tanto el string es inmutable. A diferencia de esto,
+cuando el string se declara como array, si se puede acceder a cada carácter y modificarlo, ya que el literal "Hola" sigue estando 
+en .rodata, pero al ejecutarse la función, el compilador copia byte por byte esos caracteres a la memoria Stack reservada para la variable str. */
+
+
+//EJERCICIO 9 
+//Definir una función que reciba un string y lo pase a mayúsculas. Pista: alcanza con sumar
+//la diferencia entre ’A’ y ’a’ a cada letra. Sólo aplicar la conversión si lo que llega es una
+//letra minúscula.
+
+int string_length(char *str)
+{
+    int len = 0;
+    while(*str != '\0')
+    {
+        len++;
+        str++;
+    }
+    return len;
+}
+
+
+int to_upper(char *palabra)
+{
+    int len = string_length(palabra);
+    char copia[len+1];
+    int i = 0;
+    while (*palabra != '\0')
+    {
+        if(*palabra >='a' && *palabra <='z')
+        {
+            copia[i] = (char) *palabra + ('A'-'a');
+        }
+        else
+        {
+            copia[i] = *palabra;
+        }
+        
+        printf("%c",copia[i]);
+        i++;
+        palabra++;
     }
 
+    copia[i] = '\0'; // Cerramos la cadena correctamente
+    printf("\n");
+
+    printf("%s",copia);
+
+    return 0;
+}
+
+
+int ejercicio9() 
+{
+    char *pal = "HoliS";
+    to_upper(pal);
+    return 0;
+}
+
+int funcionesSTR(){
+    // 1. strlen: Devuelve la longitud del string (sin contar el '\0').
+// Argumentos: (const char *cadena)
+size_t len = strlen("Hola"); 
+
+
+// 2. strcpy: Copia el string origen en la variable destino (sobrescribe).
+// Argumentos: (char *destino_con_espacio, const char *origen)
+char destino[50];
+strcpy(destino, "Hola"); 
+
+
+// 3. strcat: Pega el string origen al final del string destino.
+// Argumentos: (char *destino_con_espacio, const char *origen_a_pegar)
+strcat(destino, " Mundo"); 
+
+
+// 4. strcmp: Compara dos strings en orden alfabético. Devuelve 0 si son iguales.
+// Argumentos: (const char *cadena1, const char *cadena2)
+int resultado = strcmp(destino, "Hola Mundo"); // resultado es 0
+
+return 0;
+}
+
+
+//Analicemos un ejemplo de una funci´on que intenta devolver un array de enteros inicializado:
+
+uint16_t *secuencia(uint16_t n){
+    uint16_t arr[n];
+    for(uint16_t i = 0; i < n; i++){
+        arr[i] = i;
+    }
+    return arr;
+}
+
+int noFunciona(){
+    uint16_t n = 3;
+    for(int i = 0; i < n; i++){
+        printf("%d",*secuencia(n));
+    }
+}
+
+//arr es una variable automática, y por lo tanto, se libera al salir de la funci´on. Esto significa que la direcci´on de memoria a la que
+//apunta arr ya no es v´alida al salir de la funci´on
